d0f46db82b6ab2d8a09b145f79708fc0916d200f
Paul Conway <paulc00@gmail.com>
2024-11-27 18:47:15 +0000

    Add technical details to Python version

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import platform
 
-print(f"Python version: {platform.python_version()}\n")
+print(f"Python version: {platform.python_implementation()} {platform.python_version()} ({', '.join(platform.python_build())}) [{platform.python_compiler()}])\n")
 
 print("System detail:")
 for uname,uvalue in platform.uname()._asdict().items():
