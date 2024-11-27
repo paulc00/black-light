e9ff0fa04e9783387a6553bf08bd6ff8bac7c586
Paul Conway <paulc00@gmail.com>
2024-11-27 16:21:19 +0000

    Remove hello world output

--- hello.py
+++ hello.py
@@ -5,7 +5,3 @@ print(f"Python version: {platform.python_version()}\n")
 print("System detail:")
 for uname,uvalue in platform.uname()._asdict().items():
   print(f"  {uname}={uvalue}")
-
-print()
-
-print("Hello World")
